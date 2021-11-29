00ef69677f74482b62c7b7f66c3b3fd9133751a7
vladislav@book <vladovinkin@gmail.com>
2021-11-29 23:28:36 +0300

    fix: correct task update action

--- application/controllers/TaskController.php
+++ application/controllers/TaskController.php
@@ -17,39 +17,31 @@ class TaskController extends Controller {
 
 	public function updateAction() {
 		
-		if (!empty($_POST)) {
-			$task_id = $_POST['id'];
-			
-			if (isset($_POST['text'])) { // сохранение
-				$text = trim($_POST['text']);
-				$this->model->setTaskText($task_id, $text);
-				$this->view->location('/');
-				
-			} else { // нажали изменить в списке задач - переход в форму редактирования
-				$text = $this->model->getTaskText($task_id);
-				
-				if ($text) {
-					$this->view->message('Проверка', $text . ':' . $task_id);
-					// $vars = [
-						// 	'id' => $task_id,
-						// 	'text' => $text,
-						// ];
-						
-					$this->view->render('Редактирование задачи');
-				}
-
-			}
-
-
-
-
-			// $this->view->message('Ошибка авторизации', 'Неверные логин и/или пароль');
-			// $this->view->message('test-debug', $_POST['login']);
-			// $this->view->message('id = ', $_POST['id']);
-
-			// $this->model->taskCreate(trim($_POST["description"]));
-			// $this->view->location('/');
+		if (!empty($_POST) && isset($_POST['description'])) {
+			// сохранение
+			$text = trim($_POST['description']);
+			$this->model->setTaskText($this->route['id'], $text);
+			$this->view->location('/');
 		}
-		View::errorCode(403);
+
+		if (isset($this->route['id'])) {
+			$task_id = $this->route['id'];
+
+			if ($this->model->isTaskExists($task_id)) {
+				$text = $this->model->getTaskText($task_id);
+
+				$vars = [
+					'id' => $task_id,
+					'text' => $text,
+				];
+					
+				$this->view->render('Редактирование задачи', $vars);
+				
+			} else {
+				$this->view->errorCode(404);
+			}	
+		} else {
+			$this->view->errorCode(404);
+		}
 	}
 }
