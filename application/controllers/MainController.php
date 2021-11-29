317c0844a34f0fdf84238ab98f7a55bc8e91150b
vladislav@book <vladovinkin@gmail.com>
2021-11-30 02:20:34 +0300

    feat: add pagination to authorize index(tasks) page

--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -4,14 +4,18 @@ namespace application\controllers;
 
 use application\core\Controller;
 use application\models\Account;
+use application\lib\Pagination;
+
 
 class MainController extends Controller {
 
 	public function indexAction() {
-		$result = $this->model->getTasks();
+		$pagination = new Pagination($this->route, $this->model->tasksCount($this->model->getId()), 5);
+		$result = $this->model->getTasks((isset($this->route['page']) ? $this->route['page'] : 1), $pagination->getLimit());
 		$vars = [
 			'tasks' => $result,
 			'is_authorized' => Account::isAuth(),
+			'pagination' => $pagination->get(),
 		];
 		$this->view->render('Главная страница', $vars);
 	}
