0a83062ea5d89e3e708f0a259140684b7d9a7dcd
vladislav@book <vladovinkin@gmail.com>
2021-11-29 23:26:40 +0300

    fix: init update form and correct form action address link

--- application/views/task/update.php
+++ application/views/task/update.php
@@ -1,7 +1,7 @@
 <h3>Изменение задачи</h3>
-<form action="/task/update" method="post">
+<form action="/task/update/<?= $id ?>" method="post">
 	<p>Текст задачи</p>
 	<input type="hidden" name="id" value="2">
-	<p><textarea rows="3" cols="40" name="description"></textarea></p>
+	<p><textarea rows="3" cols="40" name="description"><?= $text ?></textarea></p>
 	<input type="submit" name="Сохранить изменения">
 </form>
